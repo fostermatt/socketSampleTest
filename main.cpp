4a22f12b6ebd1c776cce53ec42b02c08e5692a83
Matt Foster <matt@fostermatt.com>
2019-07-29 00:02:59 -0700

    problems with tinyxml2...

--- main.cpp
+++ main.cpp
@@ -12,7 +12,6 @@
 #include <sys/types.h> 
 #include <netinet/in.h>
 
-
 #include "tinyxml2.h"
 
 #define SERVER 0
@@ -27,7 +26,8 @@ void setServerAddress(int, char*[], string*, string*);
 void runServer(int, int);
 string readFromSocket(int);
 void writeToSocket(int, string);
-int parseInput(string);
+int parseInput(int, string, XMLDocument);
+void clearTempFiles(int);
 void error(const char *msg)
 {
     perror(msg);
@@ -35,8 +35,7 @@ void error(const char *msg)
 }
 
 int main(int argc, char * argv[]) {
-	string serverIp = "-1000";
-	string serverPort = "-1000";
+	string serverIp = "127.0.0.1", serverPort = "5000";
 
 	// Validate commandline inputs and set server IP and port
 	setServerAddress(argc, argv, &serverIp, &serverPort);
@@ -45,15 +44,6 @@ int main(int argc, char * argv[]) {
 
 	int portNo = stoi(serverPort);
 	runServer(2, portNo);
-
-	// determine if data is valid XML
-		// pass to work queue to process valid XML
-		// "Unknown Command" if invalid XML
-
-	// work queue should parse commands and display to console along with data rows
-
-	// send response to socket
-
 	
 	return 0;
 }
@@ -63,35 +53,36 @@ void argumentError() {
 	exit(0);
 }
 
-void setServerAddress(int argc, char * argv[], string* serverIp, string* serverPort){
-	if(argc == 1) {				// no address or port entered
-		// default IP and Port will be set
-	} else if (argc == 3) {		// just an address or port entered
-		if (strcmp(argv[1], "-i") == 0) {
-			*serverIp = argv[2];
-		} else if (strcmp(argv[1], "-p") == 0) {
-			*serverPort = argv[2];
-		} else {
+void setServerAddress(int argc, char * argv[], string* serverIpPointer, string* serverPortPointer){
+	switch (argc) {
+		case 1:		// no additional commandline inputs, use defaults
+			break;
+		case 3:		// just ip or port set, use default for the other
+			if (strcmp(argv[1], "-i") == 0) {
+				*serverIpPointer = argv[2];
+			} else if (strcmp(argv[1], "-p") == 0) {
+				*serverPortPointer = argv[2];
+			} else {
+				argumentError();
+			}
+			break;
+		case 5:		// set input ip and port
+			if (strcmp(argv[1], "-i") == 0 && (strcmp(argv[3], "-p") == 0)) {
+				*serverIpPointer = argv[2];
+				*serverPortPointer = argv[4];
+			} else {
+				argumentError();
+			}
+			break;
+		default: 
 			argumentError();
-		}
-		
-	} else if (argc == 5) { 	// both are entered
-		if (strcmp(argv[1], "-i") == 0 && (strcmp(argv[3], "-p") == 0)) {
-			*serverIp = argv[2];
-			*serverPort = argv[4];
-		} else {
-			argumentError();
-		}
-	} else {					// improper command line usage
-		argumentError();
 	}
-	// Set default values if not updated
-	if (*serverIp == "-1000") {
-		*serverIp = "127.0.0.1";
-	}
-	if (*serverPort == "-1000") {
-		*serverPort = "5000";
-	}
+}
+
+
+void setBothPortAndIp(char * argv[], string serverIp, string serverPort) {
+	// validate input in correct order and update values
+	
 }
 
 void runServer(int argc, int passedPortNo) {
@@ -123,7 +114,6 @@ void runServer(int argc, int passedPortNo) {
 	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT, &enable, sizeof(int)) < 0) {
 		error("setsockopt(SO_REUSEPORT) failed");
 	}
-    
 	
 	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
 		error("ERROR on binding");
@@ -139,19 +129,32 @@ void runServer(int argc, int passedPortNo) {
 	// read from socket
 	tempString = readFromSocket(newsockfd);
 
+// determine if data is valid XML
+
 	// parse XML
+	XMLDocument doc;
 	int invalidXML;
-	invalidXML = parseInput(tempString);
+	invalidXML = parseInput(newsockfd, tempString, doc);
 	if (!invalidXML) {
+		// pass to work queue
 		cout << "XML is valid" << endl;
 	} else {
 		cout << "Unknown Command" << endl;
 	}
 
+// work queue should parse commands and display to console along with data rows
+	string tempXMLfile = to_string(newsockfd)+".xml";
+	
+	doc.LoadFile(tempXMLfile.c_str());
+
+// send response to socket
 	// write to socket
 	string writeThis = "received message: \n";
 	writeToSocket(newsockfd, writeThis+tempString);
 
+
+	// cleanup temp files
+	clearTempFiles(newsockfd);
 	// release socket descriptors
 	close(newsockfd);
 	close(sockfd);
@@ -182,22 +185,20 @@ void writeToSocket(int newsockfd, string output) {
 	}
 }
 
-int parseInput(string input) {
-	tinyxml2::XMLDocument doc;
+int parseInput(int newsockfd, string input, XMLDocument doc) {
+	// tinyxml2::XMLDocument doc;
 	doc.Parse(input.c_str());
+	if(doc.ErrorID() == 0) {
+		string tempFileName = to_string(newsockfd) + ".xml";
+		doc.SaveFile( tempFileName.c_str() );
+	}
 	return doc.ErrorID();
 }
 
-
-
-
-
-
-
-
-
-
-
+void clearTempFiles(int newsockfd) {
+	string tempXMLfile = to_string(newsockfd)+".xml";
+	remove(tempXMLfile.c_str());
+}
 
 
 
